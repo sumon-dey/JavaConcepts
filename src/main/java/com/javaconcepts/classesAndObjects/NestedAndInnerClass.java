01c89b986b5df26a69d598c6e88d08140670021c
Sumon <sdtechie22@gmail.com>
2021-01-12 11:48:50 +0530

    Added more info on the Nested Classes

--- src/main/java/com/javaconcepts/classesAndObjects/NestedAndInnerClass.java
+++ src/main/java/com/javaconcepts/classesAndObjects/NestedAndInnerClass.java
@@ -5,9 +5,9 @@ package com.javaconcepts.classesAndObjects;
  * 
  * Nested Classes are classes within a class. Nested Classes can be both static
  * and non-static. Static Nested Classes are seldom used since they cannot
- * access non-static members of the outer classes. Non-Static Nested Classes are
- * known as Inner Classes and can access both static and non-static members of
- * the outer class.
+ * access non-static members of the outer class. Non-Static Nested Classes are
+ * also known as Inner Classes and can access both static and non-static members
+ * of the outer class.
  * 
  * @author Sumon Dey
  *
@@ -16,7 +16,14 @@ public class NestedAndInnerClass {
 
 	public static void main(String[] args) {
 		Shop myNewShop = new Shop();
+		myNewShop.callLargeShop();
 		myNewShop.callSmallShop();
+		/*
+		 * We cannot create objects of the nested classes directly outside the outer
+		 * class. We have to create object of the outer class and then can call only the
+		 * non-static nested (inner) classes (not static nested classes) using it.
+		 */
+		myNewShop.new SmallShop();
 	}
 
 }
@@ -25,18 +32,46 @@ public class NestedAndInnerClass {
 class Shop {
 	private String color = "Blue";
 
-	// Inner Class
-	class SmallShop {
+	// static nested class
+	/*
+	 * We have to create an object of the outer class to call a non-static member of
+	 * the outer class from inside the static nested class
+	 */
+	static class LargeShop {
 		void display() {
-			System.out.println("Inside the inner class: The color of the shop is \"" + color + "\"");
+			System.out.println("Inside the static nested class: The color of the shop is \"" + new Shop().color + "\"");
 		}
 	}
 
-	// method in the outer class which creates an instance of the inner class
-	// and calls the display method
+	// non-static nested class (Inner Class)
+	/*
+	 * We can directly call a static/non-static member of the outer class, without
+	 * creating an object of the outer class, from inside the non-static nested
+	 * class (inner class)
+	 */
+	class SmallShop {
+		void display() {
+			System.out.println(
+					"Inside the non-static nested class (inner class): The color of the shop is \"" + color + "\"");
+		}
+	}
+
+	/*
+	 * method in the outer class which creates an instance of the non-static nested
+	 * class (Inner Class) and calls its display method
+	 */
 	public void callSmallShop() {
 		SmallShop smallShop = new SmallShop();
 		smallShop.display();
+	}
+
+	/*
+	 * method in the outer class which calls the static nested class and calls its
+	 * display method
+	 */
+	public void callLargeShop() {
+		LargeShop largeShop = new LargeShop();
+		largeShop.display();
 	}
 
 }
